4d88fac7217ea482f21220e79abd2ac21753696a
Durga Chintalapudi <105465968+DurgaChintalapudi@users.noreply.github.com>
2022-12-13 21:34:01 +0000

    API update

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 '''Webapp'''
 from fastapi import FastAPI
 from fastapi.openapi.utils import get_openapi
-import dblib.dbfunctions as db
 import uvicorn
-
+import dblib.connector as db
 
 app = FastAPI()
 
 def my_schema():
+    '''Schema'''
     openapi_schema = get_openapi(
        title="Fraud Analysis",
        version="1.0",
@@ -22,6 +22,12 @@ async def root():
     '''Root'''
     return {"message": "Hello World"}
 
+#write api to get data from database
+@app.get("/getMaxTranAmount")
+async def getMaxTranAmount():
+    '''Get Max TranAmount'''
+    conn=db.Connector()
+    return conn.querydb('SELECT MAX(Tran_Amount) FROM hive_metastore.default.daily_train_cleaned_csv limit 1')
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8080, host="0.0.0.0")
